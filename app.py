066ae189e743b24635799bdae35379858bd5f6ce
Steve Radich <stever@bitshop.com>
2024-02-27 11:21:32 -0500

    feat: add health check, cleanup

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ API to send requests to be batched for S3/other storage.
 
 import os
 import logging
+import signal
 from flask import Flask, jsonify, request
 from batch_json_lines import BatchJSONLines
-import signal
 
 logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO').upper())
 logger = logging.getLogger()
@@ -37,19 +37,21 @@ else:
 
 flush_interval = 120       # pylint: disable=C0103 # This is our default value
 if "FLUSH_INTERVAL" in os.environ:
-    # check if numeric and if it's under 900 seconds we will use it. If > 900 seconds we will use 900 seconds and log a warning of config out of range
+    # check if numeric and if it's under 900 seconds we will use it.
+    # If > 900 seconds we will use 900 seconds and log a warning of config out of range
     if os.environ["FLUSH_INTERVAL"].isdigit():
         if int(os.environ["FLUSH_INTERVAL"]) < 900:
             flush_interval = int(os.environ["FLUSH_INTERVAL"])
         else:
             flush_interval = 900
-            logging.warning("FLUSH_INTERVAL env variable is set to %s, which is out of range, using 900 seconds instead", os.environ["FLUSH_INTERVAL"])
+            logging.warning("FLUSH_INTERVAL env variable is set to %s, which is out of range, using 900 seconds instead",
+                             os.environ["FLUSH_INTERVAL"])
 
 buffer_limit = 100         # pylint: disable=C0103 # This is our default value
 if "BUFFER_LIMIT" in os.environ:
     # Check for number of lines to buffer, if not numeric we will use 100 lines and log a warning
     if os.environ["BUFFER_LIMIT"].isdigit():
-        buffer_limit = (os.environ["BUFFER_LIMIT"])
+        buffer_limit = os.environ["BUFFER_LIMIT"]
 
 app = Flask(__name__)
 buffer = BatchJSONLines(prefix=prefix, buffer_limit=buffer_limit, flush_interval=flush_interval)
@@ -63,6 +65,12 @@ def send():
     buffer.add_request(request.get_json())
     return jsonify(request.get_json())
 
+@app.route("/healthy", methods=["GET"])
+def healthy():
+    """
+    Health check
+    """
+    return "OK"
 
 if __name__ == "__main__":
     app.run(debug=False, port=8000)
